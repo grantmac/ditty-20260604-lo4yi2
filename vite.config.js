76225a9224713f75880291494d13f89d4fc5b58f
Ditty <ditty@users.noreply.github.com>
2026-06-04 13:23:24 +0000

    Allow Modal preview host

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,11 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3001,
+    allowedHosts: true,
   },
   preview: {
     host: "0.0.0.0",
     port: 3001,
+    allowedHosts: true,
   },
 });
